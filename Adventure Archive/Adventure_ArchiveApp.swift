e5e61f186d6333af643fd04e957b470b241d62d7
Sanjiv Anand <58104863+sanjus-robotic-studio@users.noreply.github.com>
2024-06-22 14:01:20 +0530

    Initial Commit

---
+++ Adventure Archive/Adventure_ArchiveApp.swift
@@ -0,0 +1,32 @@
+//
+//  Adventure_ArchiveApp.swift
+//  Adventure Archive
+//
+//  Created by Sanjiv Anand on 22/06/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Adventure_ArchiveApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Adventure ArchiveUITests/Adventure_ArchiveUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Adventure_ArchiveUITestsLaunchTests.swift
+//  Adventure ArchiveUITests
+//
+//  Created by Sanjiv Anand on 22/06/24.
+//
+
+import XCTest
+
+final class Adventure_ArchiveUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
