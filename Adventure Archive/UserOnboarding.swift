ece5c209093dee846eea500df603a6234738c764
Sanjiv Anand <58104863+sanjus-robotic-studio@users.noreply.github.com>
2024-07-10 14:50:01 +0530

    Update UserOnboarding.swift

--- Adventure Archive/UserOnboarding.swift
+++ Adventure Archive/UserOnboarding.swift
@@ -62,25 +62,20 @@ struct UserOnboardingQ1: View {
                     Spacer()
                         .frame(height:20)
                     
-                    Button {
-                        
+                    NavigationLink(destination: UserOnboardingQ2().navigationBarBackButtonHidden(true)) {
+                        Text("Next")
+                            .font(.title3)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .shadow(color: Color.black.opacity(50), radius: 60)
+                            .cornerRadius(50)
+                            .foregroundColor(.white)
+                            .foregroundStyle(.white)
+                            .background{
+                                RoundedRectangle(cornerRadius: 50)
+                                    .fill(.teal)
+                            }
                     }
-                    
-                label: {
-                    Text("Next")
-                        .foregroundColor(.white)
-                }
-                .font(.title3)
-                .padding()
-                .frame(maxWidth: .infinity)
-                .shadow(color: Color.black.opacity(50), radius: 60)
-                .cornerRadius(50)
-                .foregroundColor(.white)
-                .foregroundStyle(.white)
-                .background{
-                    RoundedRectangle(cornerRadius: 50)
-                        .fill(.teal)
-                }
                     
                     
                     Spacer()
@@ -148,25 +143,20 @@ struct UserOnboardingQ2: View {
                     Spacer()
                         .frame(height:20)
                     
-                    Button {
-                        
+                    NavigationLink(destination: UserOnboardingQ3().navigationBarBackButtonHidden(true)) {
+                        Text("Next")
+                            .font(.title3)
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .shadow(color: Color.black.opacity(50), radius: 60)
+                            .cornerRadius(50)
+                            .foregroundColor(.white)
+                            .foregroundStyle(.white)
+                            .background{
+                                RoundedRectangle(cornerRadius: 50)
+                                    .fill(.teal)
+                            }
                     }
-                    
-                label: {
-                    Text("Next")
-                        .foregroundColor(.white)
-                }
-                .font(.title3)
-                .padding()
-                .frame(maxWidth: .infinity)
-                .shadow(color: Color.black.opacity(50), radius: 60)
-                .cornerRadius(50)
-                .foregroundColor(.white)
-                .foregroundStyle(.white)
-                .background{
-                    RoundedRectangle(cornerRadius: 50)
-                        .fill(.teal)
-                }
                     
                     
                     Spacer()
@@ -187,8 +177,94 @@ struct UserOnboardingQ2: View {
         }
     }
 }
-
-            
+struct UserOnboardingQ3: View {
+    @State private var timeOfDay = "Evening"
+    let options = ["Morning", "Afternoon", "Evening", "Night"  , "No preference"]
+    var body: some View {
+        NavigationView{
+            ZStack(){
+                Color(hex: "#018894").ignoresSafeArea(.all)
+                VStack{
+                    VStack(alignment: .leading ,  spacing:20){
+                        
+                        Text("Let's get to know you better.")
+                            .fontWeight(.heavy)
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                        
+                        Spacer()
+                            .frame(height:40)
+                        
+                        Text("What time of day do you prefer to journal?")
+                            .fontWeight(.heavy)
+                            .font(.title)
+                            .foregroundStyle(.white)
+                        
+                        
+                        
+                        Picker("Select an option", selection: $timeOfDay) {
+                            ForEach(options, id: \.self) { option in
+                                Text(option)
+                                    .fontWeight(.heavy)
+                                    .font(.title)
+                                    .foregroundStyle(.white)
+                            }
+                        }
+                        .pickerStyle(.wheel) // You can change the picker style if
+                        .padding()
+                        .foregroundColor(Color.white) // To make the text more visible
+                        .cornerRadius(10)
+                        .accentColor(.white)
+                        .font(.title)
+                        
+                        
+                        
+                        
+                        
+                        
+                        Spacer()
+                            .frame(height:20)
+                        
+                        Button {
+                            
+                        }
+                        
+                    label: {
+                        Text("Next")
+                            .foregroundColor(.white)
+                    }
+                    .font(.title3)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .shadow(color: Color.black.opacity(50), radius: 60)
+                    .cornerRadius(50)
+                    .foregroundColor(.white)
+                    .foregroundStyle(.white)
+                    .background{
+                        RoundedRectangle(cornerRadius: 50)
+                            .fill(.teal)
+                    }
+                        
+                        
+                        Spacer()
+                    }
+                    VStack(alignment: .trailing){
+                        HStack(alignment: .center){
+                            Image(systemName: "circle")
+                                .foregroundStyle(.white)
+                            Image(systemName: "circle")
+                                .foregroundStyle(.white)
+                            Image(systemName: "circle.fill")
+                                .foregroundStyle(.white)
+                        }
+                        .frame(alignment: .center)
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
 
 #Preview {
     UserOnboardingQ2()
